26a2efd62c01a2e4f2f6f84b6a4337c269da2ff2
Gonzalo <Gonzalo@Gonzalo>
2016-09-02 21:23:09 -0300

    creada la clase socio con gets y sets

--- CPI/src/Datos/socio.java
+++ CPI/src/Datos/socio.java
@@ -10,5 +10,178 @@ package Datos;
  * @author Gonzalo
  */
 public class socio {
+    private int id_socio;
+    private String legajo_socio;
+    private String nombre;
+    private String apellido;
+    private int dni;
+    private int telefono;
+    private String domicilio;
+    private String estado_pago;
+    private String categoria;
+    private String estado;
+    private String cuilcuit;
+    private String email;
+
+    /**
+     * @return the id_socio
+     */
+    public int getId_socio() {
+        return id_socio;
+    }
+
+    /**
+     * @return the legajo_socio
+     */
+    public String getLegajo_socio() {
+        return legajo_socio;
+    }
+
+    /**
+     * @param legajo_socio the legajo_socio to set
+     */
+    public void setLegajo_socio(String legajo_socio) {
+        this.legajo_socio = legajo_socio;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the apellido
+     */
+    public String getApellido() {
+        return apellido;
+    }
+
+    /**
+     * @param apellido the apellido to set
+     */
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    /**
+     * @return the dni
+     */
+    public int getDni() {
+        return dni;
+    }
+
+    /**
+     * @param dni the dni to set
+     */
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    /**
+     * @return the telefono
+     */
+    public int getTelefono() {
+        return telefono;
+    }
+
+    /**
+     * @param telefono the telefono to set
+     */
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    /**
+     * @return the domicilio
+     */
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    /**
+     * @param domicilio the domicilio to set
+     */
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    /**
+     * @return the estado_pago
+     */
+    public String getEstado_pago() {
+        return estado_pago;
+    }
+
+    /**
+     * @param estado_pago the estado_pago to set
+     */
+    public void setEstado_pago(String estado_pago) {
+        this.estado_pago = estado_pago;
+    }
+
+    /**
+     * @return the categoria
+     */
+    public String getCategoria() {
+        return categoria;
+    }
+
+    /**
+     * @param categoria the categoria to set
+     */
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    /**
+     * @return the estado
+     */
+    public String getEstado() {
+        return estado;
+    }
+
+    /**
+     * @param estado the estado to set
+     */
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    /**
+     * @return the cuilcuit
+     */
+    public String getCuilcuit() {
+        return cuilcuit;
+    }
+
+    /**
+     * @param cuilcuit the cuilcuit to set
+     */
+    public void setCuilcuit(String cuilcuit) {
+        this.cuilcuit = cuilcuit;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
     
 }
