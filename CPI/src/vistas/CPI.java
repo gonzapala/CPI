7e9f3b0ef98786eab3e975729a3252f7fc10822b
Gonzalo <Gonzalo@Gonzalo>
2016-09-14 18:15:09 -0300

    cambiar vista de gestion de socios y menu principal

--- CPI/src/vistas/CPI.java
+++ CPI/src/vistas/CPI.java
@@ -16,7 +16,10 @@ public class CPI {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+        MenuPrincipal ventana = new MenuPrincipal();
+        ventana.setTitle("Nueva Ventana");
+        ventana.setVisible(true);
+        ventana.setLocationRelativeTo(null);
         // un commit de prueba
         // anahi
         //bruno
